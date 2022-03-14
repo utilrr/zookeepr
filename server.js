80453bdefdf8881f24ec0042134f5f098552f169
Mark J. Healy <gmarkghealy@gmail.com>
2022-03-13 21:47:26 -0400

    Added data from the zoo

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 const { animals } = require("./data/animals");
+// Serve static pages
+app.use(express.static("public"));
 
 // functions go here
 function filterByQuery(query, animalsArray) {
@@ -102,6 +104,10 @@ app.get("/api/animals/:id", (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.post("/api/animals", (req, res) => {
